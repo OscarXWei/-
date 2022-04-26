b05b0f9468091501ec2c8de1afa5ba14c9564d2f
OscarXWei <49010185+OscarXWei@users.noreply.github.com>
2022-04-26 16:36:20 +0800

    Add files via upload

---
+++ rime.lua
@@ -0,0 +1,9 @@
+date_translator = require("date")
+week_translator = require("week")
+lunar_translator = require("lunar")
+alltime_translator = require("alltime")
+time_translator = require("time")
+number2_translator = require("number2")
+
+
+
